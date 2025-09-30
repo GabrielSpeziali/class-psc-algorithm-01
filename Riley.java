766cef478f059239a6bfc3f52734c1b6e170818c
Gabriel Speziali <130507777+GabrielSpeziali@users.noreply.github.com>
2025-09-30 17:18:13 -0300

    Add files via upload

--- Riley.java
+++ Riley.java
@@ -1,67 +1,59 @@
 import java.util.Scanner;
 
-public class Riley
-{
-	public static void main(String[] args) {
-	    int alegria = 0;
-	    int tristeza = 0;
-	    int amizades = 0;
-	    int exer = 0;
-	    int exern = 0;
-	    float a1 = 0.0f;
-	    float a2 = 0.0f;
-	    float a3 = 0.0f;
-	    float media = 0;
-	    
-	    Scanner scanner = new Scanner(System.in);
-	    
-	    //Amizades
-	    System.out.print("Quantas amizades foram feitas: ");
-	    amizades = scanner.nextInt();
-	    
-	    if (amizades > 0) {
-	        alegria = alegria + 10 * amizades;
-	    } else {
-	        tristeza = tristeza + 30;
-	    }
-	    
-	    //Cidade nova
-	    System.out.println("Qual a nota da a1: ");
-	    a1 = scanner.nextFloat();
-	    System.out.println("Qual a nota da a2: ");
-	    a2 = scanner.nextFloat();
-	    System.out.println("Qual a nota da a3: ");
-	    a3 = scanner.nextFloat();
-	    
-	    media = (a1 + a2 + a3) / 3;
-	    
-	    if (media >= 7) {
-	        alegria = alegria + 50;
-	        System.out.println("Riley está aprovada");
-	        System.out.println(alegria);
-	    } else {
-	        tristeza = tristeza + 50;
-	        System.out.println("Riley está reprovada");
-	        System.out.println(tristeza);
-	    }
-	    
-	    //Algoritmos
-	    System.out.println("Quantos exercicios feitos: ");
-	    exer = scanner.nextInt();
-	    
-	    
-	    if (exer < 10) {
-	        exern = exern = 10 - exer;
-	    } else {
-	        exern = 0;
-	    }
-	    
-	    alegria = alegria + exer * 10;
-	    tristeza = tristeza + exern * 10;
-	    
-	    System.out.println(alegria);
-	    System.out.println(tristeza);
-	    
-	    scanner.close();
-	}
+public class Riley {
+    public static void main(String[] args) {
+        int alegria = 0, tristeza = 0, amizades = 0, exer = 0, exern = 0;
+		float a1 = 0.0f, a2 = 0.0f, a3 = 0.0f, media = 0.0f;
+
+        Scanner scanner = new Scanner(System.in);
+
+		//Amizades
+        System.out.print("Quantas amizades foram feitas: ");
+        amizades = scanner.nextInt();
+        
+		if (amizades < 0) amizades = 0;
+        if (amizades > 0) {
+            alegria = alegria + 10 * amizades;
+        } else {
+            tristeza = tristeza + 30;
+        }
+        
+		//Notas
+		System.out.print("\nQual a nota da a1: ");
+        a1 = scanner.nextFloat();
+        System.out.print("Qual a nota da a2: ");
+        a2 = scanner.nextFloat();
+        System.out.print("Qual a nota da a3: ");
+        a3 = scanner.nextFloat();
+        
+		media = (a1 + a2 + a3) / 3;
+        
+		if (media >= 7) {
+            alegria = alegria + 50;
+        } else {
+            tristeza = tristeza + 50;
+        }
+        
+		//Exercicios
+		System.out.print("\nQuantos exercicios feitos: ");
+        exer = scanner.nextInt();
+        
+		if (exer < 0) exer = 0;
+        if (exer > 10) exer = 10;
+        exern = 10 - exer;
+        
+		alegria = alegria + exer * 10;
+        tristeza = tristeza + exern * 10;
+        
+		System.out.println("\nAlegria: " + alegria);
+        System.out.println("Tristeza: " + tristeza);
+       
+		if (alegria > tristeza) {
+            System.out.println("\nA mudança para a nova cidade foi uma experiência incrível para a Riley.");
+        } else {
+            System.out.println("\nA mudança para a nova cidade foi uma experiência desagradável para a Riley.");
+        }
+
+        scanner.close();
+    }
 }
